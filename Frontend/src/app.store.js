8bc07b44a71bbbb12e373b4f9e50ea8d4125909a
Chandan Kushwaha <lpu20032025@gmail.com>
2026-03-22 21:48:35 +0530

    commit

--- Frontend/src/app.store.js
+++ Frontend/src/app.store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../src/features/auth/auth.slice"
+import authReducer from "../src/features/auth/auth.slice.js"
+import chatReducer  from "../src/features/chat/chat.slice.js"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        auth:chatReducer
     }
 })
